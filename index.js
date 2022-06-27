e69d0de3367fb2b259db19fc7757dc68e5addb23
Johannes Weber <johannes@drweber.de>
2022-06-27 16:58:04 -0700

    use middleware

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
+
+
+app.use(fileUpload({createParentPath: true}));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
 const { generateLowerThird } = require('./imageFunctions');
 
